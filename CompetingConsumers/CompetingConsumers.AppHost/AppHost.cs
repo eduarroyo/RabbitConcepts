cbb9d06bdfd313a530752cb1bb154124b7115461
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2025-12-14 20:23:51 +0100

    PublisherSubscriber and refactoring

--- CompetingConsumers/CompetingConsumers.AppHost/AppHost.cs
+++ CompetingConsumers/CompetingConsumers.AppHost/AppHost.cs
@@ -5,25 +5,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var broker = builder.AddRabbitMQ("Broker")
     .WithManagementPlugin();
 
-#region Producer-Consumer
-
-var producerConsumerQueueName = "ProducerConsumer";
-var producer = builder.AddProject<ProducerConsumer_Producer>("ProducerConsumer-Producer")
-    .WithEnvironment("QUEUE_NAME", producerConsumerQueueName)
-    .WithReference(broker)
-    .WaitFor(broker);
-
-_ = builder.AddProject<ProducerConsumer_Consumer>("ProducerConsumer-Consumer")
-    .WithEnvironment("QUEUE_NAME", producerConsumerQueueName)
-    .WithReference(broker)
-    .WaitFor(producer);
-
-#endregion
-
-#region Competing consumers
-
 var competingConsumersQueueName = "CompetingConsumers";
-var producer2 = builder.AddProject<CompetingConsumers_Producer>("CompetingConsumers-Producer")
+var producer = builder.AddProject<CompetingConsumers_Producer>("CompetingConsumers-Producer")
     .WithEnvironment("QUEUE_NAME", competingConsumersQueueName)
     .WithReference(broker)
     .WaitFor(broker);
@@ -31,13 +14,11 @@ var producer2 = builder.AddProject<CompetingConsumers_Producer>("CompetingConsum
 builder.AddProject<CompetingConsumers_Consumer>("CompetingConsumers-Consumer1")
     .WithEnvironment("QUEUE_NAME", competingConsumersQueueName)
     .WithReference(broker)
-    .WaitFor(producer2);
+    .WaitFor(producer);
 builder.AddProject<CompetingConsumers_Consumer>("CompetingConsumers-Consumer2")
     .WithEnvironment("QUEUE_NAME", competingConsumersQueueName)
     .WithReference(broker)
-    .WaitFor(producer2);
-
-#endregion
+    .WaitFor(producer);
 
 var app = builder.Build();
 
--- PublisherSubscriber/PublisherSubscriber.Subscriber/Global.Usings.cs
+++ PublisherSubscriber/PublisherSubscriber.Subscriber/Global.Usings.cs
@@ -1,5 +1,5 @@
-﻿global using ProducerConsumer.Consumer;
-global using RabbitMQ.Client.Events;
+﻿global using PublisherSubscriber.Subscriber;
 global using RabbitMQ.Client;
+global using RabbitMQ.Client.Events;
 global using ServiceDefaults;
 global using System.Text;
